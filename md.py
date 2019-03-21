bec26f62d6e9abe81162ab48e39fbc62ec02d3d3
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2019-03-21 15:06:13 -0400

    add model independent md, output, struc files

---
+++ md.py
@@ -0,0 +1,91 @@
+from struc import Structure
+import numpy as np
+import time
+import datetime
+import concurrent.futures
+import md
+import output
+
+
+class MD:
+    """Generates NVE dynamics from a GP model."""
+
+    def __init__(self, force_model, dt: float, number_of_steps: int,
+                 pos_init: np.ndarray, species, cell, masses,
+                 prev_pos_init: np.ndarray=None, skip: int=0,
+                 output_name='otf_run.out'):
+
+        self.dt = dt
+        self.Nsteps = number_of_steps
+        self.force_model = force_model
+
+        self.structure = Structure(cell=cell, species=species,
+                                   positions=pos_init,
+                                   mass_dict=masses,
+                                   prev_positions=prev_pos_init)
+
+        self.noa = self.structure.positions.shape[0]
+        self.atom_list = list(range(self.noa))
+        self.curr_step = 0
+        self.kes = []
+        self.output_name = output_name
+
+    def run(self):
+        self.start_time = time.time()
+
+        while self.curr_step < self.Nsteps:
+            # verlet algorithm follows Frenkel p. 70
+            self.force_model(self.structure)  # update forces
+            self.update_positions()
+            self.record_state()
+            self.curr_step += 1
+
+        output.conclude_run(self)
+
+    def update_positions(self):
+        dtdt = self.dt ** 2
+        new_pos = np.zeros((self.noa, 3))
+
+        # update positions
+        for i, pre_pos in enumerate(self.structure.prev_positions):
+            mass = self.structure.mass_dict[self.structure.species[i]]
+            pos = self.structure.positions[i]
+            forces = self.structure.forces[i]
+
+            new_pos[i] = 2 * pos - pre_pos + dtdt * forces / mass
+
+        # update temperature
+        KE, temperature, velocities = self.calculate_temperature(new_pos)
+        self.KE = KE
+        self.temperature = temperature
+        self.velocities = velocities
+
+        self.structure.prev_positions = self.structure.positions
+        self.structure.positions = new_pos
+        self.structure.wrap_positions()
+
+    def record_state(self):
+        self.kes.append(self.KE)
+        output.write_md_config(self.dt, self.curr_step, self.structure,
+                               self.temperature, self.KE,
+                               self.start_time, self.output_name,
+                               self.velocities)
+
+    def calculate_temperature(self, new_pos):
+        # set velocity and temperature information
+        velocities = (new_pos - self.structure.prev_positions) / (2 * self.dt)
+
+        KE = 0
+        for i in range(len(self.structure.positions)):
+            for j in range(3):
+                KE += 0.5 * \
+                    self.structure.mass_dict[self.structure.species[i]] * \
+                    velocities[i][j] * velocities[i][j]
+
+        # see conversions.nb for derivation
+        kb = 0.0000861733034
+
+        # see p. 61 of "computer simulation of liquids"
+        temperature = 2 * KE / (3 * self.noa * kb)
+
+        return KE, temperature, velocities
---
+++ output.py
@@ -0,0 +1,86 @@
+import time
+import datetime
+import numpy as np
+import multiprocessing
+
+
+def write_to_output(string: str, output_file: str = 'otf_run.out'):
+    with open(output_file, 'a') as f:
+        f.write(string)
+
+
+def write_header(cutoffs, structure, output_name, std_tolerance):
+
+    with open(output_name, 'w') as f:
+        f.write(str(datetime.datetime.now()) + '\n')
+
+    # report previous positions
+    headerstring = ''
+    headerstring += '\nprevious positions (A):\n'
+    for i in range(len(structure.positions)):
+        headerstring += structure.species[i] + ' '
+        for j in range(3):
+            headerstring += str("%.8f" % structure.prev_positions[i][j]) + ' '
+        headerstring += '\n'
+    headerstring += '-' * 80 + '\n'
+
+    write_to_output(headerstring, output_name)
+
+
+def write_md_config(dt, curr_step, structure, temperature, KE,
+                    start_time, output_name, velocities):
+    string = ''
+    string += "\n*-Frame: " + str(curr_step)
+    string += '\nSimulation Time: %.3f ps \n' % (dt * curr_step)
+
+    # Construct Header line
+    string += 'El \t\t\t  Position (A) \t\t\t\t\t '
+    string += 'Force (ev/A) '
+    string += '\t\t\t\t\t\t Std. Dev (ev/A) \t'
+    string += '\t\t\t\t\t\t Velocities (A/ps) \n'
+
+    # Construct atom-by-atom description
+    for i in range(len(structure.positions)):
+        string += structure.species[i] + ' '
+        for j in range(3):
+            string += str("%.8f" % structure.positions[i][j]) + ' '
+        string += '\t'
+        for j in range(3):
+            string += str("%.8f" % structure.forces[i][j]) + ' '
+        string += '\t'
+        for j in range(3):
+            string += str("%.8e" % structure.stds[i][j]) + ' '
+        string += '\t'
+        for j in range(3):
+            string += str("%.8e" % velocities[i][j]) + ' '
+        string += '\n'
+
+    string += '\n'
+    string += 'temperature: %.2f K \n' % temperature
+    string += 'kinetic energy: %.6f eV \n' % KE
+
+    string += 'wall time from start: %.2f s \n' % \
+        (time.time() - start_time)
+
+    write_to_output(string, output_name)
+
+
+def write_hyps(hyp_labels, hyps, start_time, output_name, like, like_grad):
+    write_to_output('\nGP hyperparameters: \n', output_name)
+
+    for i, label in enumerate(hyp_labels):
+        write_to_output('Hyp{} : {} = {}\n'.format(i, label, hyps[i]),
+                        output_name)
+
+    write_to_output('likelihood: '+str(like)+'\n', output_name)
+    write_to_output('likelihood gradient: '+str(like_grad)+'\n', output_name)
+    time_curr = time.time() - start_time
+    write_to_output('wall time from start: %.2f s \n' % time_curr,
+                    output_name)
+
+
+def conclude_run(output_name):
+    footer = '▬' * 20 + '\n'
+    footer += 'Run complete. \n'
+
+    write_to_output(footer, output_name)
